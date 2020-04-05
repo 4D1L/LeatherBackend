7ffdc5519a3ee93d19b07f026dc46fe9131cece9
Adil Uddin <a.uddin@se18.qmul.ac.uk>
2020-04-05 03:20:15 +0100

    API Header
    Forgot to commit this middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt-auth' => \App\Http\Middleware\JWT::class,
+        'api-header' => \App\Http\Middleware\APIHeader::class,
     ];
 }
