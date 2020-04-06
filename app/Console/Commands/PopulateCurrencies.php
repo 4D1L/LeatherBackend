ede819f93daf6a61d04d7c6fc536ad09d330ab2e
Adil Uddin <a.uddin@se18.qmul.ac.uk>
2020-04-06 23:00:22 +0100

    Add Ethereum and Litecoin to currency populator command

--- app/Console/Commands/PopulateCurrencies.php
+++ app/Console/Commands/PopulateCurrencies.php
@@ -39,10 +39,36 @@ class PopulateCurrencies extends Command
     public function handle()
     {
         //
-        $bitcoin = new Currency();
-        $bitcoin->name = "Bitcoin";
-        $bitcoin->short = "BTC";
-        $bitcoin->transactions_supported = true;
-        $bitcoin->save();
+
+        $btcCheck = Currency::where('short', 'LIKE', 'BTC')->count();
+        if($btcCheck == 0)
+        {
+            $bitcoin = new Currency();
+            $bitcoin->name = "Bitcoin";
+            $bitcoin->short = "BTC";
+            $bitcoin->transactions_supported = true;
+            $bitcoin->save();            
+        }
+
+        $ethCheck = Currency::where('short', 'LIKE', 'ETH')->count();
+        if($ethCheck == 0)
+        {
+            $ethereum = new Currency();
+            $ethereum->name = "Ethereum";
+            $ethereum->short = "ETH";
+            $ethereum->transactions_supported = false;
+            $ethereum->save();            
+        }
+
+        $ltcCheck = Currency::where('short', 'LIKE', 'LTC')->count();
+        if($ltcCheck == 0)
+        {
+            $litecoin = new Currency();
+            $litecoin->name = "Litecoin";
+            $litecoin->short = "LTC";
+            $litecoin->transactions_supported = false;
+            $litecoin->save();            
+        }
+
     }
 }
