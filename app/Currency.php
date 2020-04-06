e79ec164b82b26edc7e5203b0e7de8cc2b57e6f9
Adil Uddin <a.uddin@se18.qmul.ac.uk>
2020-04-06 23:00:46 +0100

    Functionality to get price data about a supported currency

--- app/Currency.php
+++ app/Currency.php
@@ -16,4 +16,26 @@ class Currency extends Model
         'name', 'short', 'transactions_supported'
     ];
 
+    /**
+     * Gets currency price data from coingecko.
+     *
+     * @var array
+     */
+    public function getPriceData($name)
+    {
+        $api_url = sprintf('https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&ids=%s&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h', $name);
+        $client = new \GuzzleHttp\Client();
+        $request = $client->get($api_url);
+        $response = $request->getBody()->getContents();
+        $array = json_decode($response);
+
+        $ret["current_price"] = $array[0]->current_price;
+        $ret["market_cap"] = $array[0]->market_cap;
+        $ret["total_volume"] = $array[0]->total_volume;
+        $ret["high_24h"] = $array[0]->high_24h;
+        $ret["low_24h"] = $array[0]->low_24h;
+        $ret["price_change_24h"] = $array[0]->price_change_24h;
+        $ret["price_change_percentage_24h"] = $array[0]->price_change_percentage_24h;
+        return $ret;
+    }
 }
