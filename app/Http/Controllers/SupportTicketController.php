1641173dcb0313059fe5f7faad9b3258c1965367
Adil Uddin <a.uddin@se18.qmul.ac.uk>
2020-04-08 02:31:48 +0100

    Functionality to allow users to create tickets and messages for tickets.
    The get ticket endpoint will need to check for user role/author user id.

---
+++ app/Http/Controllers/SupportTicketController.php
@@ -0,0 +1,183 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Auth;
+use App\User;
+use App\Role\RoleChecker;
+use App\SupportTicket;
+use App\SupportTicketMessage;
+
+class SupportTicketController extends Controller
+{
+    /**
+     * Create a new SupportTicketController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    /**
+     * Returns data about a Support Ticket.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($ticketid)
+    {
+        // Fetch support ticket from the database
+        $supportTicket = SupportTicket::find($ticketid);
+
+        // If it does not exist, return error.
+        if(!$supportTicket) {
+            return response()->json([
+                'success' => false, 
+                'message' => 'Ticket not found.'
+            ], 404);
+        }
+
+        // Return the ticket.
+        /**
+         * NOTE: We do not have to manually retrieve details about the author 
+         *       and the messages associated with this ticket because of the
+         *       relationships set up between the entities.
+         *
+         */
+        $response = [
+            'success' => true,
+            'response' => [
+                "ticket" => $supportTicket
+            ]
+        ];
+        return response()->json($response);
+        
+    }
+
+    /**
+     * Creates a support ticket.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        // Validate the submission. Tickets need a ticket and an initial message.
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'message' => 'required',
+        ]);
+
+        // If validation fails, return an error message.
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'response' => [
+                    "message" => "The form is not complete."
+                ]
+            ];
+            return response()->json($response, 400);
+        }
+
+        // Create a new instance of a SupportTicket and store it in the database.
+        $supportTicket = new SupportTicket();
+        $supportTicket->title = $request->title;
+        $supportTicket->user_id = Auth::user()->id;
+        $supportTicket->save();
+
+        // Create a new instance of a support ticket message.
+        $message = new SupportTicketMessage();
+        $message->user_id = Auth::user()->id;
+        $message->content = $request->message;
+
+        // Store the message in the database, linking it to the ticket it belongs to.
+        $supportTicket->messages()->save($message);
+
+
+        // Return the new ticket and message.
+        $response = [
+            'success' => true,
+            'response' => [
+                "message" => "The support ticket was created.",
+                "ticket" => $supportTicket,
+                "ticketMessage" => $message
+            ]
+        ];
+        return response()->json($response);
+    }
+
+    /**
+     * Adds a support ticket message.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function addMessage(Request $request, $ticketid)
+    {
+        // Try to load the support ticket.
+        $supportTicket = SupportTicket::find($ticketid);
+
+        // If it does not exist, return an error.
+        if(!$supportTicket) {
+            $response = [
+                'success' => false,
+                'response' => [
+                    "message" => "The ticket does not exist."
+                ]
+            ];
+            return response()->json($response, 404);
+        }
+
+        // Create new instance of the Role Checker.
+        $roleChecker = new RoleChecker();
+
+        // Check if the authenticated user is the author of the ticket, or if they have permission to the support role.
+        if(!($supportTicket->author->id === Auth::user()->id || $roleChecker->check(Auth::user(), 'ROLE_SUPPORT')))
+        {
+            $response = [
+                'success' => false,
+                'response' => [
+                    "message" => "No permissions to make changes to this ticket."
+                ]
+            ];
+            return response()->json($response, 403);
+        }
+
+        // Validate the form.
+        $validator = Validator::make($request->all(), [
+            'message' => 'required',
+        ]);
+
+        // Else, return an error.
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'response' => [
+                    "message" => "The form is not complete."
+                ]
+            ];
+            return response()->json($response, 400);
+        }
+
+
+        // Create a new instance of a support ticket message.
+        $message = new SupportTicketMessage();
+        $message->user_id = Auth::user()->id;
+        $message->content = $request->message;
+
+        // Store the message in the database, linking it to the ticket it belongs to.
+        $supportTicket->messages()->save($message);
+
+        // Return the message object back.
+        $response = [
+            'success' => true,
+            'response' => [
+                "message" => "The message was successfully added to the ticket.",
+                "ticketMessage" => $message,
+                "ticket" => $supportTicket
+            ]
+        ];
+        return response()->json($response);
+    }
+}
