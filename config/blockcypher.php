6f1ddc6193cf2245dae73c1ce20082eef1e4e5bd
Adil Uddin <a.uddin@se18.qmul.ac.uk>
2020-04-08 18:48:27 +0100

    Update blockcypher.php

--- config/blockcypher.php
+++ config/blockcypher.php
@@ -7,7 +7,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | This key is used by the Blockcypher API.
-    | You can obtain an API key here: https://accounts.blockcypher.com/tokenss
+    | You can obtain an API key here: https://accounts.blockcypher.com/tokens
     |
     */
 
