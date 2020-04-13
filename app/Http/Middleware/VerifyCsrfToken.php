b772dddec6baf4dfd04cab7d223f15ea739ccf93
Adil Uddin <a.uddin@se18.qmul.ac.uk>
2020-04-13 03:07:40 +0100

    Update VerifyCsrfToken.php

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,11 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-<<<<<<< HEAD
         '/deploy',
         
-=======
-        //
->>>>>>> master
     ];
 }
